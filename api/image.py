d5c6e426fbaf3eec6c04f9ffc8b4d52e1e918147
punchmadedev1 <ereusmol@gmail.com>
2025-05-15 16:25:46 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -26,7 +26,7 @@ config = {
     
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
-    "message": { # Show a custom message when the user opens the image
+    "message": { ur gay
         "doMessage": True, # Enable the custom message?
         "message": "This browser has been pwned by DeKrypt's Image Logger. https://github.com/dekrypted/Discord-Image-Logger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
